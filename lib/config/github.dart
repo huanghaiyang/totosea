90960082c54f92835caf6092cc38a21ec004fd14
huanghaiyang <haiyang.huang@forceclouds.com>
2019-05-31 23:47:48 +0800

    update

---
+++ lib/config/github.dart
@@ -0,0 +1,8 @@
+final Auth = const {
+  'token': "7d58bb447daa855a5347862779528975406d0556"
+};
+
+final ApiV4 = const {
+  'host': "https://api.github.com",
+  'path': "/graphql"
+};
---
+++ lib/service/repository.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import '../config/github.dart' as githubConfig;
+
+get() async {
+  
+}
